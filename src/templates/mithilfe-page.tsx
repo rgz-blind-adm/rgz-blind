49146a4b86f5cc78c1039d6c3253b8c9a6d7c275
Rafael Robleda <raf@simpra.ch>
2023-08-30 23:49:08 +0200

    mithilfe-page.tsx

---
+++ src/templates/mithilfe-page.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
+import { Helmet } from 'react-helmet'
+
+export const MithilfePageTemplate = ({
+    title,
+    content,
+    contentComponent,
+}: {
+    title: any
+    content: any
+    contentComponent: any
+}) => {
+    const PageContent = contentComponent || Content
+
+    return (
+        <section className="section">
+            <div className="container">
+                <div className="columns">
+                    <div className="column is-10 is-offset-1">
+                        <PageContent className="content" content={content} />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+const MithilfePage = ({ data }: { data: any }) => {
+    const { mdx: post } = data
+
+    return (
+        <Layout>
+            <Helmet>
+                <title>
+                    Über uns - Schweizerischer Blindenbund Regionalgruppe Zürich
+                </title>
+                <meta
+                    name="description"
+                    content="Eine Beschreibung der Regionalgruppe Zürich des Schweizerischen Blindenbunds."
+                />
+                <meta property="og:title" content="Über uns" />
+                <meta
+                    property="og:description"
+                    content="Eine Beschreibung der Regionalgruppe Zürich des Schweizerischen Blindenbunds."
+                />
+                <meta
+                    property="og:url"
+                    content="https://www.rgz-blind.ch/mithilfe/"
+                />
+            </Helmet>
+            <MithilfePageTemplate
+                contentComponent={HTMLContent}
+                title={post.frontmatter.title}
+                content={post.body}
+            />
+        </Layout>
+    )
+}
+
+export default MithilfePage
+
+export const mithilfePageQuery = graphql`
+    query MithilfePage($id: String!) {
+        mdx(id: { eq: $id }) {
+            body
+            frontmatter {
+                title
+            }
+        }
+    }
+`
